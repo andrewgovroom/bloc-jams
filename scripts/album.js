40e3a199cd02f4147d45e6055875fa716317f871
Andrew <andrewgovroom@hotmail.com>
2017-02-05 17:06:57 -0800

    Assignment 13 Play Pause Part2: completed

--- scripts/album.js
+++ scripts/album.js
@@ -68,8 +68,16 @@ var albumPicasso = {
 var findParentByClassName = function(element, targetClass) {
     if (element) {
         var currentParent = element.parentElement;
+        
+        if(!currentParent){
+            console.log('No parent found');
+        }
+        
         while (currentParent.className != targetClass && currentParent.className !== null) {
             currentParent = currentParent.parentElement;
+            if(!currentParent) {
+                console.log('No parent found with that class name.');
+            }
         }
         return currentParent;
     }
