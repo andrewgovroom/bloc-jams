1544940d90d17aadebfbe05f1b4d85ea3193e684
Andrew <andrewgovroom@hotmail.com>
2017-02-13 19:43:10 -0800

    Updated Play/Pause Butoon on bar

--- scripts/album.js
+++ scripts/album.js
@@ -291,6 +291,22 @@ var previousSong = function() {
 };
 
 
+var togglePlayFromPlayerBar = function(){
+    
+
+  if (currentSoundFile.isPaused()) {
+        $(getSongNumberCell(currentlyPlayingSongNumber)).html(pauseButtonTemplate);
+        $('.main-controls .play-pause').html(playerBarPauseButton);
+        currentSoundFile.play();
+    } else {
+        $(getSongNumberCell(currentlyPlayingSongNumber)).html(playButtonTemplate);
+        $('.main-controls .play-pause').html(playerBarPlayButton);
+        currentSoundFile.pause();   
+    }
+	       
+    
+    
+}
 
 
 var playButtonTemplate = '<a class="album-song-button"><span class="ion-play"></span></a>';
@@ -306,12 +322,13 @@ var currentVolume = 80;
 
 var $previousButton = $('.main-controls .previous');
 var $nextButton = $('.main-controls .next');
-
+var $playButton = $('.main-controls .play-pause');
 
   $(document).ready(function() {
      setCurrentAlbum(albumPicasso);
      setupSeekBars();
      $previousButton.click(previousSong);
      $nextButton.click(nextSong);
+     $playButton.click(togglePlayFromPlayerBar); 
 
 });
